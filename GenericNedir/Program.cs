b9058ba4fff856477249f85d6fe8226e7498de2a
Kadir Dağlı <mehmetkadirdagli27@gmail.com>
2024-01-07 13:16:05 +0300

    Proje dosyası ekle.

---
+++ GenericNedir/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericNedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //bir datayı bir liste içerisinde tutucaksak ve bunu tabiki generic olarak tutmak isteriz
+            // ve list genericleri yoğun olarak kullanılırız
+            // listgeneric arraylistin generic versiyonu
+          
+            // <t>  t tip demek .net framwork içerisinde olan veya bizim oluşturduğumuz
+            //  herhangi bir tip demek.
+            List<int> list = new List<int>();
+            list.Add(1);
+            list.Add(2);
+            for(int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            List<string> isimler = new List<string>();
+            isimler.Add("cengiz");
+            isimler.Add("kadir");
+            for (int i = 0; i < isimler.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
+
+            List<msuteri> msuteriListe = new List<msuteri>();
+            msuteriListe.Add(new msuteri()
+            {
+                id = 1,
+                isim = "cengiz",
+                soyisim = "daglı"
+
+            } );
+            foreach( msuteri item in msuteriListe)
+            {
+                Console.WriteLine(item.isim);
+            }
+            Console.ReadLine();
+        }
+    }
+}
